b4535b76b50129b5b7faceceb42d8948d94d2b6b
Pratibha Shah <pratibhashah1995@gmail.com>
2024-09-18 19:10:57 +0530

    new code

--- js/script2.js
+++ js/script2.js
@@ -126,38 +126,38 @@ document.addEventListener('DOMContentLoaded', function () {
   }, 100);
 });
 
-    
+
 // Toggle arrow direction
 function toggleFilter(label) {
-    const icon = label.querySelector('.icon-container i');
-    const filterInput = label.nextElementSibling; // Assuming the input follows the label
-    const filterList = filterInput.nextElementSibling; // Assuming the ul follows the input
+  const icon = label.querySelector('.icon-container i');
+  const filterInput = label.nextElementSibling; // Assuming the input follows the label
+  const filterList = filterInput.nextElementSibling; // Assuming the ul follows the input
 
 
-    if (icon.classList.contains('fa-angle-down')) {
-        icon.classList.remove('fa-angle-down');
-        icon.classList.add('fa-angle-up');
-    } else {
-        icon.classList.remove('fa-angle-up');
-        icon.classList.add('fa-angle-down');
-    }
+  if (icon.classList.contains('fa-angle-down')) {
+    icon.classList.remove('fa-angle-down');
+    icon.classList.add('fa-angle-up');
+  } else {
+    icon.classList.remove('fa-angle-up');
+    icon.classList.add('fa-angle-down');
+  }
 
-    // Toggle the visibility of the search input and filter list
-    if (filterInput.style.display === 'none' || !filterInput.style.display) {
-        filterInput.style.display = 'block';
-        filterList.style.display = 'block';
-    } else {
-        filterInput.style.display = 'none';
-        filterList.style.display = 'none';
-    }
+  // Toggle the visibility of the search input and filter list
+  if (filterInput.style.display === 'none' || !filterInput.style.display) {
+    filterInput.style.display = 'block';
+    filterList.style.display = 'block';
+  } else {
+    filterInput.style.display = 'none';
+    filterList.style.display = 'none';
+  }
 }
 
 
 
 //print 
 function printWindow() {
-    // Directly open the print dialog
-    window.print();
+  // Directly open the print dialog
+  window.print();
 }
 
 
@@ -175,19 +175,19 @@ document.getElementById('search-button').addEventListener('click', function () {
   // Toggle the search container visibility
   const searchContainer = document.getElementById('search-container');
   searchContainer.classList.toggle('hidden');
-  
+
   // Focus the search input when shown
   if (!searchContainer.classList.contains('hidden')) {
-      document.getElementById('search-input').focus();
+    document.getElementById('search-input').focus();
   }
 });
 
 document.getElementById('search-input').addEventListener('input', function () {
   const clearIcon = document.getElementById('clear-icon');
   if (this.value.trim() !== '') {
-      clearIcon.classList.remove('hidden');
+    clearIcon.classList.remove('hidden');
   } else {
-      clearIcon.classList.add('hidden');
+    clearIcon.classList.add('hidden');
   }
 });
 
@@ -264,25 +264,25 @@ document.addEventListener('DOMContentLoaded', function () {
   const toggleSections = document.querySelectorAll('.building-permissions-parent');
 
   toggleSections.forEach(section => {
-      section.addEventListener('click', function () {
-          const barChartContainer = this.nextElementSibling; // Select the next sibling which is the bar-chart-container
+    section.addEventListener('click', function () {
+      const barChartContainer = this.nextElementSibling; // Select the next sibling which is the bar-chart-container
 
-          // Toggle active class to change the icon and show/hide content
-          this.classList.toggle('active');
+      // Toggle active class to change the icon and show/hide content
+      this.classList.toggle('active');
 
-          // Toggle display of the bar-chart-container
-          if (barChartContainer.style.display === 'block') {
-              barChartContainer.style.display = 'none';
-              this.querySelector('.toggle-icon').classList.replace('fa-angle-up', 'fa-angle-down');
-          } else {
-              barChartContainer.style.display = 'block';
-              this.querySelector('.toggle-icon').classList.replace('fa-angle-down', 'fa-angle-up');
-          }
-      });
+      // Toggle display of the bar-chart-container
+      if (barChartContainer.style.display === 'block') {
+        barChartContainer.style.display = 'none';
+        this.querySelector('.toggle-icon').classList.replace('fa-angle-up', 'fa-angle-down');
+      } else {
+        barChartContainer.style.display = 'block';
+        this.querySelector('.toggle-icon').classList.replace('fa-angle-down', 'fa-angle-up');
+      }
+    });
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const component6 = document.querySelector('.component-6');
   const component7 = document.querySelector('.component-7');
   const images = document.querySelectorAll('.ui-iconbookmarklight'); // Select all images
@@ -290,7 +290,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Define colors
   const defaultColor6 = '#f0f0f0'; // Default background color for component-6
   const activeColor6 = 'radial-gradient(50% 50% at 50% 50%, #198038, #24a148)'; // Active background color for component-6
-  
+
   const defaultColor7 = '#f0f0f0'; // Default background color for component-7
   const activeColor7 = 'radial-gradient(50% 50% at 50% 50%, #198038, #24a148)'; // Active background color for component-7
 
@@ -299,7 +299,7 @@ document.addEventListener('DOMContentLoaded', function() {
   component6.style.color = 'white'; // Set initial text color for better contrast
 
   // Click event for component-6
-  component6.addEventListener('click', function() {
+  component6.addEventListener('click', function () {
     // Keep component-6 active when clicked
     component6.style.background = activeColor6;
     component6.style.color = 'white'; // Maintain text color for better contrast
@@ -313,7 +313,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Click event for component-7
-  component7.addEventListener('click', function() {
+  component7.addEventListener('click', function () {
     // Set background color for component-7 to active
     component7.style.background = activeColor7;
     component7.style.color = 'white'; // Change text color to white for better contrast
@@ -335,24 +335,24 @@ function closeFilters() {
 }
 
 //cluster legend 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const clusterLegend = document.getElementById('cluster_legend');
   const toggleButton = document.getElementById('toggleClusterLegend');
   const map = document.getElementById('map'); // Assuming your map has an id of 'map'
 
-  toggleButton.addEventListener('click', function(event) {
+  toggleButton.addEventListener('click', function (event) {
     // Toggle the 'no-border' class to remove or add border
     this.classList.toggle('no-border');
 
     // Toggle the visibility of the cluster legend
     clusterLegend.classList.toggle('hidden');
-    
+
     // Prevent click event from bubbling up to the map
     event.stopPropagation();
   });
 
   // Close the cluster legend when clicking on the map
-  map.addEventListener('click', function() {
+  map.addEventListener('click', function () {
     // Add the 'hidden' class to close the legend if it's visible
     if (!clusterLegend.classList.contains('hidden')) {
       clusterLegend.classList.add('hidden');
@@ -460,24 +460,173 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-document.getElementById('toggle-select').addEventListener('click', function() {
-    const searchContainer = document.getElementById('search-container');
-    const selectElement = document.getElementById('search_type');
-    
-    // Toggle the display of the select element
-    if (selectElement.style.display === 'none' || selectElement.style.display === '') {
-        selectElement.style.display = 'block';
-    } else {
-        selectElement.style.display = 'none';
-    }
+document.getElementById('toggle-select').addEventListener('click', function () {
+  const searchContainer = document.getElementById('search-container');
+  const selectElement = document.getElementById('search_type');
+
+  // Toggle the display of the select element
+  if (selectElement.style.display === 'none' || selectElement.style.display === '') {
+    selectElement.style.display = 'block';
+  } else {
+    selectElement.style.display = 'none';
+  }
 });
 
 // Optional: Close the select element if clicked outside
-document.addEventListener('click', function(event) {
-    const toggleSelect = document.getElementById('toggle-select');
-    
-    // Close dropdown if clicked outside
-    if (!searchContainer.contains(event.target) && !toggleSelect.contains(event.target)) {
-        selectElement.style.display = 'none';
-    }
+document.addEventListener('click', function (event) {
+  const toggleSelect = document.getElementById('toggle-select');
+
+  // Close dropdown if clicked outside
+  if (!searchContainer.contains(event.target) && !toggleSelect.contains(event.target)) {
+    selectElement.style.display = 'none';
+  }
 });
+
+
+// calender
+
+
+
+document.getElementById('calendar-icon').addEventListener('click', function () {
+  let calendarElement = document.querySelector('.flatpickr-calendar');
+  if (calendarElement) {
+    calendarElement.remove();
+    return;
+  }
+
+  let calendarContainer = document.createElement('div');
+  calendarContainer.id = 'calendar-container';
+  document.getElementById('date-range-display').appendChild(calendarContainer);
+
+  flatpickr(calendarContainer, {
+    mode: "range",
+    inline: true,
+    defaultDate: ["2024-04-01", "2025-03-31"],
+    onClose: function (selectedDates) {
+      let formattedDates = selectedDates.map(date => formatDate(date)).join(' to ');
+      document.getElementById('date-range-display').firstChild.textContent = formattedDates;
+      handleDateRange(selectedDates);
+    },
+    onReady: function () {
+      let calendarFooter = document.createElement('div');
+      calendarFooter.className = 'flatpickr-custom-buttons';
+      calendarFooter.innerHTML = `
+        <button id="cancel-button" class="flatpickr-button">Apply</button>
+        <button id="apply-button" class="flatpickr-button">Choose Date</button>
+      `;
+      document.querySelector('.flatpickr-calendar').appendChild(calendarFooter);
+
+      document.getElementById('cancel-button').addEventListener('click', function () {
+        document.querySelector('.flatpickr-calendar').remove();
+      });
+
+      document.getElementById('apply-button').addEventListener('click', function () {
+        instance.close();
+      });
+
+      updateWeekdayLabels();
+    }
+  });
+});
+
+function formatDate(date) {
+  let options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
+
+function handleDateRange(selectedDates) {
+  const startDate = selectedDates[0].toISOString().split('T')[0];
+  const endDate = selectedDates[1].toISOString().split('T')[0];
+
+  const cql_filter = `conc_appr_ >= '${startDate}' AND conc_appr_ < '${endDate}'`;
+  console.log('CQL Filter:', cql_filter);
+
+  loadFilteredData(cql_filter);
+}
+
+function loadFilteredData(cql_filter) {
+  const main_url = "https://iwmsgis.pmc.gov.in/geoserver/";
+  const filterNames = ["entry_timestamp"];
+
+  filterNames.forEach(filterName => {
+    const url = `${main_url}AutoDCR/wms?service=WFS&version=1.1.0&request=GetFeature&typeName=Plot_Layout&propertyName=${filterName}&outputFormat=application/json&cql_filter=${encodeURIComponent(cql_filter)}`;
+    console.log(url);
+    $.getJSON(url, function(data) {
+      displayDataOnMap(data);
+    });
+  });
+}
+
+function displayDataOnMap(data) {
+  map.eachLayer(layer => {
+    if (layer instanceof L.GeoJSON) {
+      map.removeLayer(layer);
+    }
+  });
+
+  L.geoJSON(data, {
+    style: function(feature) {
+      return { color: feature.properties.color || 'blue' };
+    },
+    onEachFeature: function(feature, layer) {
+      if (feature.properties && feature.properties.name) {
+        layer.bindPopup(feature.properties.name);
+      }
+    }
+  }).addTo(map);
+}
+
+function updateWeekdayLabels() {
+  let weekdays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+  let weekdayElements = document.querySelectorAll('.flatpickr-weekday');
+  weekdayElements.forEach((element, index) => {
+    if (element.textContent.trim() !== '') {
+      element.textContent = weekdays[index];
+    }
+  });
+}
+
+
+// Function to toggle the visibility of the search input
+function toggleSearchInput() {
+  const searchButton = document.getElementById('search-button');
+  const searchInput = document.getElementById('searchInputDashboard');
+  const searchIcon = document.getElementById('search-icon');
+  const closeIcon = document.getElementById('close-icon');
+
+  // Toggle the full-width class on the button
+  searchButton.classList.toggle('full-width');
+
+  // Toggle the visibility of the search input field and icons
+  if (searchButton.classList.contains('full-width')) {
+    searchInput.style.display = 'block';
+    searchIcon.style.display = 'none';
+    closeIcon.style.display = 'inline';
+    searchInput.focus();
+  } else {
+    searchInput.style.display = 'none';
+    searchIcon.style.display = 'inline';
+    closeIcon.style.display = 'none';
+  }
+}
+
+// Function to handle closing the search input when clicking outside of it
+function handleClickOutside(event) {
+  const searchButton = document.getElementById('search-button');
+  const searchInput = document.getElementById('searchInputDashboard');
+  const searchIcon = document.getElementById('search-icon');
+  const closeIcon = document.getElementById('close-icon');
+
+  // Check if the click is outside the search button and input
+  if (!searchButton.contains(event.target) && !searchInput.contains(event.target) && !closeIcon.contains(event.target)) {
+    // Close search input if it's visible
+    if (searchButton.classList.contains('full-width')) {
+      searchButton.classList.remove('full-width');
+      searchInput.style.display = 'none';
+      searchIcon.style.display = 'inline';
+      closeIcon.style.display = 'none';
+    }
+  }
+}
+
+
